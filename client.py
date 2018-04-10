9150fbce7b7070c723d152c0b44666dd023a10d9
gracjang <graku12@gmail.com>
2018-04-10 21:26:27 +0200

    Refresh

--- client.py
+++ client.py
@@ -1,16 +1,41 @@
+"""Gracjan Grochowski ISSP"""
+import sys
 import socket
+
+""" Variables to open socket server"""
 host = ''
-port = 5000
+port = 3200
 
 mySocket = socket.socket()
-mySocket.connect((host, port))
-message = input("> ")
 
+
+try:
+    mySocket.connect((host, port))
+    print("Connected to server !")
+    message = input("> ")
+except:
+    print("Couldnt connect with the socket-server:")
+    sys.exit(1)
+"""Loop over message quit"""
 while message != 'quit':
-    mySocket.send(message.encode())
-    data = mySocket.recv(1024).decode()
-    print('Message: ' + str(data[6:]))
-    message = input(">> ")
-
-
+    try:
+        mySocket.send(message.encode())
+        data = mySocket.recv(1024).decode()
+        print('Message: ' + str(data[6:]))
+        message = input(">> ")
+    except:
+        print("ERROR")
+print('\r','\n','>>CONNECTION CLOSE<<','\r','\n')
 mySocket.close()
+
+
+"""Example : """
+""" > python3 serwer.py &
+    > python3 client.py
+    > lower INF
+    > Message inf
+    > upper inf
+    > message INF
+    > quit
+    > Connection CLOSE
+ """
--- serwer.py
+++ serwer.py
@@ -1,25 +1,32 @@
+"""Gracjan Grochowski ISSP"""
 import socket
+import time
 host = ''
-port = 5000
-
+port = 3200
+"""Open socket server"""
 mySocket = socket.socket()
 mySocket.bind((host, port))
-
+"""Listen socket and accept connection"""
 mySocket.listen(1)
 conn, addr = mySocket.accept()
+time.sleep(2)
 print("Connection from: " + str(addr))
+
 print("Server using :\n1. quit \n2. lower [word]\n3. upper [word]")
-
+"""Main lopp when conn is True and converting data from client"""
 while True:
-    data = conn.recv(1024).decode()
-    if not data:
-        break
-    if str(data[:5]) == "lower":
-        conn.send(data.encode().lower())
-    elif str(data[:5]) == "upper":
-        conn.send(data.encode().upper())
-    elif str(data[:5]) != "upper" and str(data[:5]) != "lower":
-        data = "      ERROR ! \nServer using : \n1. quit \n2. lower \n3. upper"
-        conn.send(data.encode())
+    try:
+        data = conn.recv(1024).decode()
+        if not data:
+            break
+        if str(data[:5]) == "lower":
+            conn.send(data.encode().lower())
+        elif str(data[:5]) == "upper":
+            conn.send(data.encode().upper())
+        elif str(data[:5]) != "upper" and str(data[:5]) != "lower" or str(data) == " ":
+            data = "      ERROR ! \nServer using : \n1. quit \n2. lower \n3. upper"
+            conn.send(data.encode())
 
+    except:
+        print("ERROR")
 conn.close()
